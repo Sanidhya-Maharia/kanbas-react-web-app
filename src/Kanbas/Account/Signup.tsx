336e7ff2ed94c925857c88b0e29686ce63531888
Sanidhya Maharia <sanidhyamaharia@gmail.com>
2024-10-02 03:32:48 -0400

    Assignment page look update

--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -4,10 +4,10 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" /><br/>
-      <input placeholder="password" type="password" /><br/>
-      <input placeholder="verify password" type="password" /><br/>
-      <Link to="/Kanbas/Account/Profile" > Sign up </Link><br />
+      <input placeholder="Username" className="form-control mb-2"/>
+      <input placeholder="Password" type="password" className="form-control mb-2"/>
+      <input placeholder="Verify password" type="password" className="form-control mb-2"/>
+      <Link to="/Kanbas/Account/Profile" className="btn btn-primary w-100"> Sign up </Link>
       <Link to="/Kanbas/Account/Signin" >Sign in</Link>
     </div>
 );}
