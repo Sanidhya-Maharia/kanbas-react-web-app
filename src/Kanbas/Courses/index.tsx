def59538979b775854857de71c032cb656dad1b1
Sanidhya Maharia <sanidhyamaharia@gmail.com>
2024-12-04 18:59:33 -0500

    assignments part left, everything else done

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,4 +1,5 @@
 import { Route, Navigate, Routes, useParams, useLocation } from "react-router";
+import { useState, useEffect } from "react";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
@@ -6,10 +7,20 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa6";
 import PeopleTable from "./People/Table";
+import * as client from "./client";
 export default function Courses({ courses }: { courses: any[]; }) {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
   const { pathname } = useLocation();
+  const [users, setUsers] = useState<any[]>([]);
+  const fetchUsers = async (cid: any) => {
+    const users = await client.findUsersForCourse(cid);
+    setUsers(users);
+  };
+  const { uid } = useParams();
+  useEffect(() => {
+    fetchUsers(cid);
+  }, [uid]);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
@@ -27,7 +38,7 @@ export default function Courses({ courses }: { courses: any[]; }) {
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-            <Route path="People" element={<PeopleTable />} />
+            <Route path="People" element={<PeopleTable users={users} />} />
           </Routes>
         </div>
       </div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -16,32 +16,70 @@ import * as userClient from "./Account/client";
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const fetchCourses = async () => {
-    let courses = [];
+  const [enrolling, setEnrolling] = useState<boolean>(false);
+  const findCoursesForUser = async () => {
     try {
-      //courses = await userClient.findMyCourses();
-      courses = await courseClient.fetchAllCourses();
+      const courses = await userClient.findCoursesForUser(currentUser._id);
+      setCourses(courses);
     } catch (error) {
       console.error(error);
     }
-    setCourses(courses);
+  };
+  const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+    if (enrolled) {
+      await userClient.enrollIntoCourse(currentUser._id, courseId);
+    } else {
+      await userClient.unenrollFromCourse(currentUser._id, courseId);
+    }
+    setCourses(
+      courses.map((course) => {
+        if (course._id === courseId) {
+          return { ...course, enrolled: enrolled };
+        } else {
+          return course;
+        }
+      })
+    );
+  };
+  const fetchCourses = async () => {
+    try {
+      const allCourses = await courseClient.fetchAllCourses();
+      const enrolledCourses = await userClient.findCoursesForUser(
+        currentUser._id
+      );
+      const courses = allCourses.map((course: any) => {
+        if (enrolledCourses.find((c: any) => c._id === course._id)) {
+          return { ...course, enrolled: true };
+        } else {
+          return course;
+        }
+      });
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
   };
   useEffect(() => {
-    fetchCourses();
-  }, [currentUser]);
+    if (enrolling) {
+      fetchCourses();
+    } else {
+      findCoursesForUser();
+    }
+  }, [currentUser, enrolling]);
   const [course, setCourse] = useState<any>({
     _id: "0", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
     image: "course.jpg", description: "New Description"
   });
 
-  const { enrollments } = useSelector((state: any) => state.enrollmentReducer);
-  const dispatch = useDispatch();
+  //const { enrollments } = useSelector((state: any) => state.enrollmentReducer);
+  //const dispatch = useDispatch();
 
   const addNewCourse = async () => {
-    const newCourse = await userClient.createCourse(course);
-    setCourses([...courses, newCourse ]);
-    dispatch(addEnrollment({ user: currentUser._id, course: newCourse._id }))
+    //const newCourse = await userClient.createCourse(course);
+    const newCourse = await courseClient.createCourse(course);
+    setCourses([...courses, newCourse]);
+    //dispatch(addEnrollment({ user: currentUser._id, course: newCourse._id }))
   };
 
   const deleteCourse = async (courseId: string) => {
@@ -78,7 +116,10 @@ export default function Kanbas() {
                   setCourse={setCourse}
                   addNewCourse={addNewCourse}
                   deleteCourse={deleteCourse}
-                  updateCourse={updateCourse} />
+                  updateCourse={updateCourse}
+                  enrolling={enrolling}
+                  setEnrolling={setEnrolling}
+                  updateEnrollment={updateEnrollment} />
               </ProtectedRoute>} />
             <Route path="/Courses/:cid/*" element={
               <ProtectedRoute>
